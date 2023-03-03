328c9a2aa71bfd6c13a07c826f48372193f19247
jemisha.vargas <jemisha.vargas@oit.edu>
2023-03-02 17:37:16 -0800

    Initial commit

---
+++ RemindRx/RemindRx/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RemindRx.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RemindRx.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ RemindRx/RemindRx/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RemindRx.Models;
+using RemindRx.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RemindRx.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
