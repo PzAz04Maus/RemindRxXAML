9ed86155b5bdd897c3a6df11dfc56714bb307f76
Stephen Carter <stephen.carter@oit.edu>
2023-03-05 05:04:09 -0800

    2 example popups mocked up. and ready

--- RemindRx/RemindRx/Views/AboutPage.xaml.cs
+++ RemindRx/RemindRx/Views/AboutPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,6 +11,18 @@ namespace RemindRx.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+        //SRC: https://github.com/xamarin/xamarin-forms-samples/blob/main/Navigation/Pop-ups/WorkingWithPopups/ActionSheetPage.xaml.cs
+        //Quick suggestion suggests an async task ... function() to handle events ... for any event. Do not fall for its tricks, the below is fine - Clippy fails
+        async void btnAlertModal_Click(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+
+        async void OnActionSheetClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Debug.WriteLine("Action: " + action);
         }
     }
 }
