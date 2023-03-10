6f109e7024dcaaf0d518ff06e4fd970f1b6840fa
Stephen Carter <stephen.carter@oit.edu>
2023-03-10 08:59:55 -0800

    Fixed Contacts list failing to initialize

--- RemindRx/RemindRx/Views/ItemsPage.xaml.cs
+++ RemindRx/RemindRx/Views/ItemsPage.xaml.cs
@@ -1,14 +1,5 @@
-﻿using RemindRx.Models;
-using RemindRx.ViewModels;
-using RemindRx.Views;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RemindRx.ViewModels;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace RemindRx.Views
 {
